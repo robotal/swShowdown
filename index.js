8108a56c0577273f3095fc35e5b17de5fad215d1
Tal <tal.davidi@gmail.com>
2017-06-03 15:34:09 -0400

    basic server structure set up

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
 var express = require('express');
-
-
-
-
-
+var bodyParser = require('body-parser');
+var logger = require('morgan');
+var exphbs = require('express-handlebars');
+var _ = require('underscore');
 
 var app = express();
+
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended : false}));
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+app.use('/public', express.static('public'));
+
+app.get('/', function(req,res){
+    res.render('index');
+});
+
+app.listen(3000, function(){
+    console.log('sw-showdown listening on port 3000');
+});
